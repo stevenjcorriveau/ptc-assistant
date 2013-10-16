3c16d27b4fc667229abbe9807d904b374d51be76
stevenjcorriveau <stevenjcorriveau@gmail.com>
2013-10-16 12:11:47 -0400

    Fixed GitHub issue #15.

--- profiles/ptc_assistant/themes/contrib/ptc_origins/template.php
+++ profiles/ptc_assistant/themes/contrib/ptc_origins/template.php
@@ -356,79 +356,78 @@ function ptc_origins_css_alter(&$css) {
  * will not render.
  */
 function ptc_origins_links($variables) {
-  $links = $variables['links'];
-  $attributes = $variables['attributes'];
-  $heading = $variables['heading'];
-  global $language_url;
-  $output = '';
+	$output = '';
+	$links = $variables['links'];
+	$attributes = $variables['attributes'];
+	$heading = $variables['heading'];
+	global $language_url;
 
-  if (count($links) > 0) {
-    $output = '';
+	if (count($links) > 0) {
+		$output = '';
 
-    // Treat the heading first if it is present to prepend it to the
-    // list of links.
-    if (!empty($heading)) {
-      if (is_string($heading)) {
-        // Prepare the array that will be used when the passed heading
-        // is a string.
-        $heading = array(
-          'text' => $heading,
-          // Set the default level of the heading.
-          'level' => 'h2',
-        );
-      }
-      $output .= '<' . $heading['level'];
-      if (!empty($heading['class'])) {
-        $output .= drupal_attributes(array('class' => $heading['class']));
-      }
-      $output .= '>' . check_plain($heading['text']) . '</' . $heading['level'] . '>';
-    }
+		// Treat the heading first if it is present to prepend it to the
+		// list of links.
+		if (!empty($heading)) {
+			if (is_string($heading)) {
+				// Prepare the array that will be used when the passed heading
+				// is a string.
+				$heading = array(
+					'text' => $heading,
+					// Set the default level of the heading.
+					'level' => 'h2',
+				);
+			}
+			$output .= '<' . $heading['level'];
+			if (!empty($heading['class'])) {
+				$output .= drupal_attributes(array('class' => $heading['class']));
+			}
+			$output .= '>' . check_plain($heading['text']) . '</' . $heading['level'] . '>';
+		}
 
-    $output .= '<ul' . drupal_attributes($attributes) . '>';
+		$output .= '<ul' . drupal_attributes($attributes) . '>';
 
-    $num_links = count($links);
-    $i = 1;
+		$num_links = count($links);
+		$i = 1;
 
-    foreach ($links as $key => $link) {
-      if (!isset($link['#access']) || $link['#access'] !== FALSE) {
-        $class = array($key);
+		foreach ($links as $key => $link) {
+			if (!isset($link['#access']) || $link['#access'] !== FALSE) {
+				$class = array($key);
 
-        // Add first, last and active classes to the list of links to help out themers.
-        if ($i == 1) {
-          $class[] = 'first';
-        }
-        if ($i == $num_links) {
-          $class[] = 'last';
-        }
-        if (isset($link['href']) && ($link['href'] == $_GET['q'] || ($link['href'] == '<front>' && drupal_is_front_page())) && (empty($link['language']) || $link['language']->language == $language_url->language)) {
-          $class[] = 'active';
-        }
-        $output .= '<li' . drupal_attributes(array('class' => $class)) . '>';
+				// Add first, last and active classes to the list of links to help out themers.
+				if ($i == 1) {
+					$class[] = 'first';
+				}
+				if ($i == $num_links) {
+					$class[] = 'last';
+				}
+				if (isset($link['href']) && ($link['href'] == $_GET['q'] || ($link['href'] == '<front>' && drupal_is_front_page())) && (empty($link['language']) || $link['language']->language == $language_url->language)) {
+					$class[] = 'active';
+				}
+				$output .= '<li' . drupal_attributes(array('class' => $class)) . '>';
 
-        if (isset($link['href'])) {
-          // Pass in $link as $options, they share the same keys.
-          $output .= l($link['title'], $link['href'], $link);
-        }
-        elseif (!empty($link['title'])) {
-          // Some links are actually not links, but we wrap these in <span> for adding title and class attributes.
-          if (empty($link['html'])) {
-            $link['title'] = check_plain($link['title']);
-          }
-          $span_attributes = '';
-          if (isset($link['attributes'])) {
-            $span_attributes = drupal_attributes($link['attributes']);
-          }
-          $output .= '<span' . $span_attributes . '>' . $link['title'] . '</span>';
-        }
+				if (isset($link['href'])) {
+					// Pass in $link as $options, they share the same keys.
+					$output .= l($link['title'], $link['href'], $link);
+				}
+				elseif (!empty($link['title'])) {
+					// Some links are actually not links, but we wrap these in <span> for adding title and class attributes.
+					if (empty($link['html'])) {
+						$link['title'] = check_plain($link['title']);
+					}
+					$span_attributes = '';
+					if (isset($link['attributes'])) {
+						$span_attributes = drupal_attributes($link['attributes']);
+					}
+					$output .= '<span' . $span_attributes . '>' . $link['title'] . '</span>';
+				}
 
-        $i++;
-        $output .= "</li>\n";
-      }
-    }
+				$i++;
+				$output .= "</li>\n";
+			}
+		}
 
-    $output .= '</ul>';
-  }
-
+		$output .= '</ul>';
+	}
   return $output;
 }
 
