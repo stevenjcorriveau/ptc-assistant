be293693bc450b6eae748caaa8b3b3c8e0306ee2
stevenjcorriveau <stevenjcorriveau@gmail.com>
2014-07-06 15:23:41 -0400

    Created ptc_assistant subtheme off of pixture_reloaded.

---
+++ profiles/ptc_assistant/themes/custom/ptc_assistant/template.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your ptc_assistant theme.
+ * 
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function ptc_assistant_preprocess_html(&$vars) {
+}
+function ptc_assistant_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function ptc_assistant_preprocess_page(&$vars) {
+}
+function ptc_assistant_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function ptc_assistant_preprocess_node(&$vars) {
+}
+function ptc_assistant_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function ptc_assistant_preprocess_comment(&$vars) {
+}
+function ptc_assistant_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function ptc_assistant_preprocess_block(&$vars) {
+}
+function ptc_assistant_process_block(&$vars) {
+}
+// */
